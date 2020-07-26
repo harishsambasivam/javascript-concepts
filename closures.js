39c9f08c05914a5a46c4bd977015882dbd7bb067
harishsambasivam <imharishsambasivam@gmail.com>
2020-07-26 20:00:20 +0530

    fixed #1

--- closures.js
+++ closures.js
@@ -1,15 +1,15 @@
-function greet(greeting){
-    return function(name){
-        console.log(greeting + name);
-    }
+function greet(greeting) {
+  return function (name) {
+    console.log(greeting + " " + name);
+  };
 }
 
-let  greetHi = greet('Hi');
-greetHi('Everyone.'); // Hi Everyone.
+let greetHi = greet("Hi");
+greetHi("Everyone."); // Hi Everyone.
 
 // Call Stack stores the function call to execute.
 
-// |                |  
+// |                |
 // |                |
 // |                |
 // |________________|
@@ -17,7 +17,7 @@ greetHi('Everyone.'); // Hi Everyone.
 // |____Context_____|
 
 // |                |
-// |________________|               
+// |________________|
 // |    greet()     |  // Execution context of
 // |  greeting="hi" |     greet
 // |________________|
@@ -27,20 +27,19 @@ greetHi('Everyone.'); // Hi Everyone.
 
 // |    greetHi()   | // Execution context of greetHi.
 // |name="everyone" |
-// |________________|               
+// |________________|
 // | greeting="hi"  |  // Execution Context is poped off but greeting is still in memory.
 // |________________|
 // |Global Execution|
 // |____Context_____|
 
-// |    greetHi()   | 
+// |    greetHi()   |
 // |name="everyone" | -------.
-// |________________|        |   Eventhough greet function finshes execution    
-// | greeting="hi"  | <------'   any function created inside still have reference 
+// |________________|        |   Eventhough greet function finshes execution
+// | greeting="hi"  | <------'   any function created inside still have reference
 // |________________|            to greet's memory and it's not garbage collected.
 // |Global Execution|
 // |____Context_____|
-
 
 // This phenomenon of closing in all the variables it supposed to have access to
 //  is called closure
@@ -58,8 +57,6 @@ greetHi('Everyone.'); // Hi Everyone.
 //     return functions;
 // }
 
-
-
 // function foo(){
 //     let functions = [];
 //     for(let i=0;i<3;i++){
@@ -72,21 +69,18 @@ greetHi('Everyone.'); // Hi Everyone.
 //     return functions;
 // }
 
-
-function foo(){
-    var functions = [];
-    for(var i=0;i<3;i++){
-        functions.push(
-       (function(j){
-            return (
-                function(){
-                    console.log(j)
-                }
-            )
-        }(i))
-      )
-    }
-        return functions;
+function foo() {
+  var functions = [];
+  for (var i = 0; i < 3; i++) {
+    functions.push(
+      (function (j) {
+        return function () {
+          console.log(j);
+        };
+      })(i)
+    );
+  }
+  return functions;
 }
 
 let fns = foo();
